af9e586f41c250037a6b8fddf7f51a50de288273
Fabien Mackowiak <fmackodev@gmail.com>
2020-04-03 20:07:23 +0200

    cors fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ require("dotenv/config");
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.fabienmackowiak.com');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
