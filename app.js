5bf2b417983abfc69995a2e994036f429f4d60ba
Fabien Mackowiak <fmackodev@gmail.com>
2020-11-01 12:32:16 +0100

    dev env

--- app.js
+++ app.js
@@ -9,9 +9,27 @@ const config = require('./config');
 
 require("dotenv/config");
 
-var cors = require('cors')
+// Middlewares in express
 
-app.use(cors()) // Use this after the variable declaration
+// Allow all
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use(bodyParser.json());
 app.use("/comments", commentsRoute);
